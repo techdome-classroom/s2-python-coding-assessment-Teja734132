e175da8a06d7828971fb0946d96b3332426ce390
Teja734132 <vmaruthi234@gmail.com>
2024-11-11 16:00:34 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -6,8 +6,7 @@ class Solution(object):
         
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         
-        # Sum the value of each character and handle subtraction rule based on the pairwise comparison
-        return sum((-1 if d[a] < d[b] else 1) * d[a] for a, b in pairwise(s)) + d[s[-1]]
+         return sum((-1 if d[a] < d[b] else 1) * d[a] for a, b in pairwise(s)) + d[s[-1]]
         pass
 
 
