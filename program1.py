07b35492742acbdc689a99db2d8eabb231dcf8a0
Teja734132 <vmaruthi234@gmail.com>
2024-11-11 15:52:51 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,9 +1,15 @@
 class Solution(object):
     def isValid(self, s):
-        """
-        :type s: str
-        :rtype: bool
-        """
+        class Solution:
+    def isValid(self, s: str) -> bool:
+        stk = []
+        d = {'()', '[]', '{}'}
+        for c in s:
+            if c in '({[':
+                stk.append(c)
+            elif not stk or stk.pop() + c not in d:
+                return False
+        return not stk
         pass
 
 
